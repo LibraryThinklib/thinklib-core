4304f53c962069b54753bceed3922186177faf7a
Waldevv <133806522+Waldevv@users.noreply.github.com>
2025-06-12 16:11:30 -0400

    Ajustes

--- Assets/Joystick Pack/Scripts/Collectible/GameManager.cs
+++ Assets/Joystick Pack/Scripts/Collectible/GameManager.cs
@@ -32,7 +32,7 @@ public class GameManager : MonoBehaviour
                     if (vida != null)
                     {
                         Debug.Log("Chamando GanharVida()");
-                        vida.GanharVida(valor);
+                        vida.Heal(valor);
                     }
                 }
                 break;
