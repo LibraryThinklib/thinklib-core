3acb5421a93a86ff7280a68f1877ebb4b52cfeac
Tiago Farias Barbosa <93674466+manupbastos@users.noreply.github.com>
2025-07-22 17:08:55 -0300

    feat: mecânica do inventário

---
+++ Assets/Thinklib/PointAndClick/scripts/Itens.cs
@@ -0,0 +1,16 @@
+// Item.cs
+using UnityEngine;
+
+// The annotation below allows creating instances of this object in the Unity menu
+// Assets > Create > Point and Click > Item
+[CreateAssetMenu(fileName = "NewItem", menuName = "Point and Click/Item")]
+public class Item : ScriptableObject
+{
+    [Header("Item Information")]
+    public string name;
+    [TextArea(3, 5)] // To give the description field more space in the Inspector
+    public string description;
+
+    [Header("Inventory Visuals")]
+    public Sprite icon;
+}
